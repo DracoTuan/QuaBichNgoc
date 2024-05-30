e95914f218c57dcfcbcdf90689492c1978e16d7c
DracoTuan <92090565+DracoTuan@users.noreply.github.com>
2024-05-30 18:19:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,42 @@
-// luckywheel animation with Greensock  ---------------------------------
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #f0f0f0;
+}
 
-$(document).ready(function() {
-    //  Setup variables
-    var wheel = $(".wheel"),
-        active = $(".active"),
-        currentRotation,
-        lastRotation = 0,
-        tolerance,
-        deg,
-        
-        $btnPlay = $("#btnPlay"),
-        $btnSlowMo = $("#btnSlowMo");
-    
-    //  Random degree
-        function getRandomInt(min, max) {
-          return Math.floor(Math.random() * (max - min + 1)) + min;
-        }
-        var deg = getRandomInt(360, 1080);
-        console.log(deg);
-    
-    //  Creating the Timeline
-        var indicator = new TimelineMax();
-        var spinWheel = new TimelineMax();
-        indicator.to(active, .13, {rotation: -10, transformOrigin:"65% 36%", ease:Power1.easeOut})
-                 .to(active, .13, {rotation: 3, ease:Power4.easeOut})
-                 .add("end");
-    
-    //  Luckywheel animation
-        spinWheel.to(wheel, 5, {rotation: deg, transformOrigin:"50% 50%", ease:Power4.easeOut, onUpdate: (
-        function(){    
-          currentRotation = Math.round(this.target[0]._gsTransform.rotation);    //_gsTransform: current position of the wheel
-          tolerance = currentRotation - lastRotation;
-          
-            console.log("lastRot: "+lastRotation);
-            console.log("currentRot: "+currentRotation);
-            console.log("tol: "+tolerance);
-            console.log(indicator.progress());
-            console.log("spinwheelprogress: "+spinWheel.progress());
-          
-          if(Math.round(currentRotation) % (360/12) <= tolerance){
-            if(indicator.progress() > .2 || indicator.progress() === 0){
-              indicator.play(0);
-            }
-          }
-          lastRotation = currentRotation;
-        }
-        )});
-        spinWheel.add("end");
-     //   Buttons
-      $btnPlay.click(
-        function(){
-          indicator.timeScale(1).seek(0);
-          spinWheel.timeScale(1).seek(0);
-        }
-      );
-    
-      $btnSlowMo.click(
-        function(){
-          indicator.timeScale(.2).seek(.5);
-          spinWheel.timeScale(.2).seek(.5);
-        }
-      );
-    });
-    
+.wheel-container {
+    text-align: center;
+}
+
+.wheel {
+    width: 300px;
+    height: 300px;
+    border-radius: 50%;
+    border: 5px solid #333;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.segment {
+    position: absolute;
+    width: 50%;
+    height: 50%;
+    background-color: #ff0000;
+    border: 1px solid #333;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-size: 24px;
+}
+
+.segment:nth-child(2) {
+    transform: rotate(180deg) translateX(100%);
+    background-color: #00ff00;
+}
